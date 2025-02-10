a263b7d927a3b85e85ae492319734e324c9ab464
Eline Kupers <elinekupers@gmail.com>
2025-02-10 15:09:31 -0600

    under construction -- load specific images for texture ptb

--- functions/vcd_getRunImagesToLoad.m
+++ functions/vcd_getRunImagesToLoad.m
@@ -1,16 +1,37 @@
-function cond_order = vcd_getRunConditionOrder(p)
+function im_order = vcd_getRunImagesToLoad(subj_session, images, p)
 
 
 %% Get the trial order for this run
-p.run_order = []
+im_order = cell(2,6);
 
 
-
+for ii = 1:length(subj_session)
     
+    st_crossing = subj_session(ii).name;
+    tmp = strsplit(st_crossing,'-');
+    taskClass = tmp{1}; 
+    stimClass = tmp{2}; 
     
+    % check we assigned the string to the right label
+    assert(any(strcmp(stimClass,p.exp.taskClassLabels)))
+    assert(any(strcmp(stimClass,p.exp.stimClassLabels)))
     
-
-
+    for jj = 1:length(subj_session(ii).trial)
+    
+        subj_session(ii).trial(jj).unique_im_nr
+    
+        im = images.(stimClass);
+        
+        switch stimClass
+            case 'rdk'
+                t = readtable(params.stim.rdk.infofile);
+                t(subj_session(ii).trial(jj).unique_im_nr,:)
+                assert 
+        end
+        
+    end
+    
+end
 
 
 return
