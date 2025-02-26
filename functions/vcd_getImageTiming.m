ad4a82ff58e33c6b726c0f5edb96651578111e82
Eline Kupers <elinekupers@gmail.com>
2025-02-26 10:55:30 -0600

    add timing of within trial events

--- functions/vcd_getImageTiming.m
+++ functions/vcd_getImageTiming.m
@@ -1,4 +1,4 @@
-function timing = vcd_getImageTiming(params, subj_run) 
+function timing = vcd_getImageTiming(params, subj_run, run_image_order,im_seq_order) 
 
 % Fixation order and fixation
 fixsoafun = @() round(params.stim.fix.dotmeanchange + (params.stim.fix.dotchangeplusminus*(2*(rand-.5))))*params.stim.fps;
@@ -21,26 +21,116 @@ for bb = 1:size(cellblock,2)
 
     tmp_timing = cellblock{6,bb};
     im_nr = tmp_timing.unique_im;
-    
     if ~iscell(im_nr)
         % convert to cell
         im_nr = num2cell(im_nr);
     end
 
     % check for nans
-    for xi = 1:length(im_nr)
-        tmp_im = im_nr{xi};
-        if isnan(tmp_im)
-            tmp_im(isnan(tmp_im))=0;
-            im_nr{xi} = tmp_im;
-        end
-        clear tmp_im
-    end  
-    seq_stim = cat(1, seq_stim, im_nr);
+    if sum(cell2mat(cellfun(@isempty, cellfun(@isnan, im_nr,'UniformOutput',false),'UniformOutput',false)))>0
+        for xi = 1:length(im_nr)
+            tmp_im = im_nr{xi};
+            if isnan(tmp_im)
+                tmp_im(isnan(tmp_im))=0;
+                im_nr{xi} = tmp_im;
+            end
+            clear tmp_im
+        end  
+    end
+    
+    if bb == 1
+        cumultime = 0;
+    end
+    % add second stim if it is a double epoch trial
+    for tt = 1:length(im_nr)
+        
+        if im_nr{tt}==0 % blockstart
+            seq_stim = cat(1, seq_stim, im_nr(tt));
+            seq_timing = cat(1,seq_timing, cumultime);
+            cumultime = cumultime + tmp_timing.event_dur(tt);
+            
+        elseif im_nr{tt}==97 % 97: task cue
+            seq_stim = cat(1, seq_stim, im_nr(tt));
+            seq_timing = cat(1,seq_timing, cumultime);
+            cumultime = cumultime + tmp_timing.event_dur(tt);
+            
+        elseif any(im_nr{tt}==([98,99])) % 98: iti or 99: ibi
+            seq_stim = cat(1, seq_stim, im_nr(tt));
+            seq_timing = cat(1,seq_timing, cumultime);
+            
+            cumultime = cumultime + tmp_timing.event_dur(tt);
 
-    seq_timing = cat(1,seq_timing, tmp_timing.run_time);
-    spatial_cue = cat(1,spatial_cue, tmp_timing.spatial_cue);
+        % 93: response ID // 94: trial_start_ID // 95: spatial_cue_ID // 96:delay_ID = 96    
+        else 
+            idx = tt/2;
+            assert(isequal(cell2mat(squeeze(im_seq_order(bb,idx,:,1)))', im_nr{tt}))
+            
+            trial_start_im       = {params.exp.miniblock.trial_start_ID}; % 96: trial start 
+            trial_start_run_time = cumultime;
+            cumultime            = cumultime + params.exp.trial.start_cue_dur;
+            
+            spatial_cue_im       = {params.exp.miniblock.spatial_cue_ID};
+            spatial_cue_run_time = cumultime;
+            cumultime            = cumultime + params.exp.trial.spatial_cue_dur;
+            
+            stim_im              = im_nr(tt);
+            stim_run_time        = cumultime; 
+            cumultime = cumultime + params.exp.trial.stim_array_dur;
+
+            
+            if cellblock{5,bb} == 2
+                delay_im            = {params.exp.miniblock.delay_ID};
+                delay_run_time      = cumultime; 
+                cumultime           = cumultime + params.exp.trial.delay_dur; 
+                
+                query_im            = {catcell(2,squeeze(im_seq_order(bb,idx,:,2)))};
+                query_run_time      = cumultime; 
+                cumultime           = cumultime + params.exp.trial.stim_array_dur;
+                
+                response_im         = {params.exp.miniblock.response_ID};
+                response_run_time   = cumultime; 
+                cumultime           = cumultime + params.exp.trial.response_win_dur;
+                
+                seq_stim = cat(1, seq_stim, ...
+                    trial_start_im,...
+                    spatial_cue_im, ...
+                    stim_im, ...
+                    delay_im,...
+                    query_im, ...
+                    response_im);
+
+                seq_timing = cat(1,seq_timing, ...
+                    trial_start_run_time, ...
+                    spatial_cue_run_time, ...
+                    stim_run_time, ...
+                    delay_run_time, ...
+                    query_run_time, ...
+                    response_run_time);
+            else
+                response_im = {params.exp.miniblock.response_ID};
+                response_run_time = cumultime;
+                cumultime = cumultime + params.exp.trial.response_win_dur;
+                
+                seq_stim = cat(1, seq_stim, ...
+                    trial_start_im,...
+                    spatial_cue_im, ...
+                    stim_im, ...
+                    response_im);
+
+                seq_timing = cat(1,seq_timing, ...
+                    trial_start_run_time, ...
+                    spatial_cue_run_time, ...
+                    stim_run_time, ...
+                    response_run_time);
+            end
+            spatial_cue = cat(1,spatial_cue, [spatial_cue_im, tmp_timing.spatial_cue(tt)]); % same as params.exp.trial.spatial_cue_dur?;
+        end
+       
+    end
 end
+
+seq_stim  = cat(1, seq_stim, 0);
+seq_timing = cat(1, seq_timing, cumultime);
 
 timing.seq_stim        = seq_stim;
 timing.seq_spatial_cue = spatial_cue;
