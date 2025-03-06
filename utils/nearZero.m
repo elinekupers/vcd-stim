9fc4033537201f05d3a8f6537fad465496547486
elinekupers <elinekupers@gmail.com>
2025-03-06 08:34:10 -0800

    lower tolerance

--- utils/nearZero.m
+++ utils/nearZero.m
@@ -1,7 +1,7 @@
 function status = nearZero(x,tolerance)
 
 if ~exist('tolerance','var') || isempty(tolerance)
-    tolerance = 10.^-10;
+    tolerance = 1.0^-10;
 end
 
 status = abs(x) < tolerance;
