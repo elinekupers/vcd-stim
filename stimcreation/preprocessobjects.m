e3649b3894a85bbcd545486ff0e1c843eb8b445f
Eline Kupers <elinekupers@gmail.com>
2025-03-13 14:10:44 -0700

    add knk preprocess objects script

---
+++ stimcreation/preprocessobjects.m
@@ -0,0 +1,240 @@
+%% DEAL WITH CONSTANTS
+
+% define
+dir0 = '/var/fairstate-ext2/GoogleDrive/VCD/experimental_design/stimuli/workspaces/complex_objects/images_to_process';
+numrot = 22;        % number of image viewpoints
+numobj = 16;        % number of objects
+targetsize = 354;   % number of pixels for one side of conformed square
+targetmnlum = 0.5;  % desired mn-luminance for grand average of one object's viewpoints
+targetsdlum = 0.03; % desired sd-luminance for grand average of one object's viewpoints
+squaresizes = 2:2:1000;  % square sizes (in pixels for one side) to evaluate
+squarethresh = .9;  % square size is chosen that includes at least 90% of the mass (of the mean mask)
+figuredir = '/var/fairstate-ext2/GoogleDrive/VCD/experimental_design/stimuli/workspaces/complex_objects/tryD_targetsdlum_0pt03';  % where diagnostic figures are written
+outputdir = [figuredir '/finalimages'];  % where final images are written to (this directory is REMOVED if it exists)
+
+%% LOAD IMAGES
+
+% find the images
+files0 = matchfiles([dir0 '/*.png']);
+assert(length(files0)==numrot*numobj);
+
+% load all images
+%   (images: convert to grayscale and then double. becomes 0-255.)
+%   (alpha: convert to double. becomes 0-255.)
+allimages = zeros(1024,1024,length(files0));
+allalphas = zeros(1024,1024,length(files0));
+for p=1:length(files0), p
+  [im0,~,alpha0] = imread(files0{p});
+  allimages(:,:,p) = double(rgb2gray(im0));
+  allalphas(:,:,p) = double(alpha0);
+end
+
+% initialize final output
+allimages2 = zeros(1024,1024,length(files0),'uint8');
+allalphas2 = zeros(1024,1024,length(files0),'uint8');
+validpct = zeros(1,length(files0));
+OF = zeros(1,numobj);
+SC = zeros(1,numobj);
+
+% make dirs
+rmdirquiet(outputdir);
+mkdirquiet(outputdir);
+mkdirquiet(figuredir);
+
+%% PROCESS THE IMAGES
+
+% loop over each object
+for zz=1:numobj
+
+  %% EXTRACT STUFF
+
+  % extract images for the object and convert to luminance (assuming squaring)
+  ims = (allimages(:,:,(zz-1)*numrot + (1:numrot))/255).^2;  % now in range 0-1
+  
+  % extract alphas and convert to fraction between 0-1
+  als = allalphas(:,:,(zz-1)*numrot + (1:numrot))/255;       % now in range 0-1 (1 means object, 0 means background)
+  
+  %% CENTER BASED ON CENTER-OF-MASS
+  
+  % figure out center of each image and shift the image and alpha to be centered at 512.5.
+  % note the rounding (hence, image pixel values don't actually get perturbed).
+  for yy=1:size(als,3)
+    com = centerofmass(als(:,:,yy),[1 2]);  % [row; col] in decimal matrix coordinates
+    ims(:,:,yy) = circshift(ims(:,:,yy),round(512.5-com)');  % be careful about the signs!!!
+    als(:,:,yy) = circshift(als(:,:,yy),round(512.5-com)');
+  end
+  
+  %% FIGURE OUT SQUARE SIZE
+  
+  % prepare mean mask and compute total mass
+  meanmask = mean(als,3);
+  tot0 = sum(flatten(meanmask));  % total mass
+    
+  % determine conformed square size
+  rec = [];
+  for ss=1:length(squaresizes)
+    crop = 513-squaresizes(ss)/2 : 512+squaresizes(ss)/2;
+    rec(ss) = sum(flatten(meanmask(crop,crop)));
+  end
+  finalsqsz = squaresizes(firstel(find(rec/tot0 > squarethresh)));  % number of pixels on a side
+  
+  % visualize the results!!
+  crop = 513-finalsqsz/2 : 512+finalsqsz/2;
+  figureprep([100 100 800 800]); hold on;
+  imagesc(meanmask); axis image tight;
+  plotrectangle([crop(1)-0.5 crop(end)+0.5 crop(1)-0.5 crop(end)+0.5],'r-');
+  set(gca,'YDir','reverse');
+  figurewrite(sprintf('obj%02d',zz),[],[],figuredir);
+
+  %% APPLY SCALING SUCH THAT SQUARE IS DESIRED SIZE
+  
+  % how much scale factor do we want?
+  S = targetsize/finalsqsz;
+  
+  % apply scaling (note that rounding to integer pixels occurs and placement rounding occurs!)
+  for yy=1:size(ims,3)
+    im0 = normalizerange(imresize(ims(:,:,yy),S,'bicubic'),0,1,0,1);
+    al0 = normalizerange(imresize(als(:,:,yy),S,'bicubic'),0,1,0,1);
+    ims(:,:,yy) = placematrix(zeros(1024,1024),im0,[]);
+    als(:,:,yy) = placematrix(zeros(1024,1024),al0,[]);
+  end
+  
+  % check that we didn't enlarge too much
+  assert(allzero(flatten(als([1 end],:,yy))));
+  assert(allzero(flatten(als(:,[1 end],yy))));
+  
+  %% DEAL WITH MEAN AND SD OF LUMINANCE
+  
+  % compute mean of each image (within the alpha mask)
+  mns = [];
+  for yy=1:size(ims,3)
+    mask0 = als(:,:,yy) > 0.5;  % note simple hack
+    im0 = ims(:,:,yy);
+    mns(yy) = mean(im0(mask0));
+  end
+
+  % figure out adjustment factor for mean
+  OF(zz) = targetmnlum - mean(mns);  % what offset to add to ensure grand mean of mean-luminance meets the target      
+
+  % after dealing with mean, compute sd of each image (use the whole image!!)
+  sds = [];
+  for yy=1:size(ims,3)
+
+    % apply and mix
+    newim0 = ims(:,:,yy) + OF(zz);
+    newim0 = newim0 .* als(:,:,yy) + ims(:,:,yy) .* (1-als(:,:,yy));  % mix according to alpha (to avoid background going crazy)
+    
+    % make composite image
+    comp0 = newim0 .* als(:,:,yy) + targetmnlum * (1-als(:,:,yy));
+    
+    % record
+    sds(yy) = std(comp0(:));
+
+  end
+
+  % figure out initial guess for adjustment factor for std
+  SC(zz) = targetsdlum / mean(sds);  % what scale to apply to ensure grand mean of std-luminance meets the target
+  
+  % loop until we are happy
+  while 1
+  
+    % apply adjustment factors and make RGB images again
+    imsAFTER = zeros(size(ims));
+    for yy=1:size(ims,3)
+    
+      % apply and mix
+      newim0 = SC(zz) * (ims(:,:,yy) - mns(yy)) + mns(yy) + OF(zz);  % adjust scale and then adjust offset
+      newim0 = newim0 .* als(:,:,yy) + ims(:,:,yy) .* (1-als(:,:,yy));  % mix according to alpha (to avoid background going crazy)
+    
+      % count percentage of OK pixels
+      mask0 = als(:,:,yy) > 0.5;
+      validpct((zz-1)*numrot + yy) = sum(newim0(mask0) >= 0 & newim0(mask0) <= 1) / sum(mask0(:)) * 100;
+    
+      % truncate
+      newim0(newim0 < 0) = 0;
+      newim0(newim0 > 1) = 1;
+    
+      % record
+      imsAFTER(:,:,yy) = newim0;
+      allimages2(:,:,(zz-1)*numrot + yy) = uint8(1+254*sqrt(newim0));   %uint8(255*sqrt(newim0));
+      allalphas2(:,:,(zz-1)*numrot + yy) = uint8(255*als(:,:,yy));
+
+    end
+
+    % compute empirical std devs
+    imtemp = allimages2(:,:,(zz-1)*numrot + (1:numrot));
+    altemp = allalphas2(:,:,(zz-1)*numrot + (1:numrot));
+    temp = ((double(imtemp)-1)/254).^2 .* double(altemp)/255 + targetmnlum * (1-double(altemp)/255);
+    estds = std(squish(temp,2),[],1);
+    
+    % if mean is within 1% of targetsdlum, we are done
+    if abs(mean(estds)-targetsdlum) / targetsdlum < .01
+      break;
+    end
+
+    % if we are off, apply guess for the update
+    fprintf('**** ANOTHER (%d) ****\n',zz);
+    SC(zz) = SC(zz) * (targetsdlum / mean(estds));
+
+  end
+
+  % visualize the histogram before and after
+  figureprep([100 100 500 800]);
+  subplot(2,1,1); hold on; hist(ims(als > 0.5),100); xlim([0 1]);
+  subplot(2,1,2); hold on; hist(imsAFTER(als > 0.5),100); xlim([0 1]);
+  figurewrite(sprintf('hist%02d',zz),[],[],figuredir);
+    
+end
+
+% write images to disk
+for ii=1:size(allimages2,3)
+  imwrite(allimages2(:,:,ii),[outputdir '/' stripfile(files0{ii},1)],'Alpha',allalphas2(:,:,ii));
+end
+
+% visualize
+figureprep;
+bar(OF,1);
+figurewrite('OF',[],[],figuredir);
+
+% visualize
+figureprep;
+bar(SC,1);
+figurewrite('SC',[],[],figuredir);
+
+% visualize
+figureprep([100 100 900 300]);
+plot(validpct,'r.-');
+figurewrite('validpct',[],[],figuredir);
+
+% visualize
+temp = ((double(allimages2)-1)/254).^2 .* double(allalphas2)/255 + targetmnlum * (1-double(allalphas2)/255);
+emeans = mean(squish(temp,2),1);
+estds = std(squish(temp,2),[],1);
+figureprep([100 100 800 400]);
+subplot(2,1,1); hold on;
+plot(emeans);
+straightline((1:numrot:numrot*numobj)-.5,'v','k-');
+title('Empirical means');
+subplot(2,1,2); hold on;
+plot(estds);
+straightline((1:numrot:numrot*numobj)-.5,'v','k-');
+title('Empirical std devs');
+figurewrite('empirical',[],[],figuredir);
+
+% save workspace (except for some large variables)
+saveexcept([figuredir '/record.mat'],{'allimages' 'allalphas' 'allimages2' 'allalphas2' 'ims' 'imsAFTER' 'als' 'temp' 'imtemp' 'altemp'});
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+% ixtouse = [1:91];  % 
+% 
+%   sqim = zeros(1024,1024);
+%   sqim(crop,crop) = 1;
+% 
+% %  [params,R2] = fitgaussian2d(mean(als,3),[],1);
+% %  [xx,yy] = meshgrid(1:1024,1:1024);
+% %  subplot(1,2,2); imagesc(evalgaussian2d(params,xx,yy)); axis image tight;
+%   figure;
+%   subplot(1,2,1); imagesc(meanmask); axis image tight;
+%   subplot(1,2,2); imagesc(sqim); axis image tight;
+% 
