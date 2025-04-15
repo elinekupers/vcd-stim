febe847cfd446947f2a8b13ac0cc652c7added44
Eline Kupers <elinekupers@gmail.com>
2025-04-14 19:26:16 -0500

    remove is_cued from ns stimuli, we will add this column later

--- bookkeeping/vcd_addCatchTrials.m
+++ bookkeeping/vcd_addCatchTrials.m
@@ -16,10 +16,10 @@ if isequal(unique(condition_table_in.stimloc),[1;2])
     tmp_table.task_class(new_row_idx)      = condition_table_in.task_class(end-1);
     tmp_table.task_class_name(new_row_idx) = condition_table_in.task_class_name(end-1);
 
-    tmp_table.iscatch(new_row_idx)         = true(2*n_catch_trials,1);
+    tmp_table.is_catch(new_row_idx)         = true(2*n_catch_trials,1);
     
     if strcmp(condition_table_in.task_class_name,{'fix'})
-        tmp_table.iscued(new_row_idx)         = NaN(length(new_row_idx),1);
+        tmp_table.is_cued(new_row_idx)         = NaN(length(new_row_idx),1);
     else
     
         if exist('cue_dir','var')
@@ -28,20 +28,20 @@ if isequal(unique(condition_table_in.stimloc),[1;2])
             % We will allocate cuing pseudo-randomly
             if numel(cue_dir) == 1
                 if cue_dir == 1 % left
-                    tmp_table.iscued(new_row_idx(1)) = 1;
-                    tmp_table.iscued(new_row_idx(2)) = 0;
+                    tmp_table.is_cued(new_row_idx(1)) = 1;
+                    tmp_table.is_cued(new_row_idx(2)) = 0;
                 elseif cue_dir == 2 % right
-                    tmp_table.iscued(new_row_idx(2)) = 1;
-                    tmp_table.iscued(new_row_idx(1)) = 0;
+                    tmp_table.is_cued(new_row_idx(2)) = 1;
+                    tmp_table.is_cued(new_row_idx(1)) = 0;
                 end
             elseif numel(cue_dir) > 1
                 for kk = 1:length(cue_dir)
                     if cue_dir(kk) == 1 % left
-                        tmp_table.iscued(new_row_idx(1+((kk-1)*2))) = 1;
-                        tmp_table.iscued(new_row_idx(2+((kk-1)*2))) = 0;
+                        tmp_table.is_cued(new_row_idx(1+((kk-1)*2))) = 1;
+                        tmp_table.is_cued(new_row_idx(2+((kk-1)*2))) = 0;
                     elseif cue_dir(kk) == 2 % right
-                        tmp_table.iscued(new_row_idx(1+((kk-1)*2))) = 0;
-                        tmp_table.iscued(new_row_idx(2+((kk-1)*2))) = 1;
+                        tmp_table.is_cued(new_row_idx(1+((kk-1)*2))) = 0;
+                        tmp_table.is_cued(new_row_idx(2+((kk-1)*2))) = 1;
                     end
                 end
             end
@@ -51,11 +51,11 @@ if isequal(unique(condition_table_in.stimloc),[1;2])
                 cue_dir = shuffle_concat([1,2],n_catch_trials/2)';
                 for kk = 1:length(cue_dir)
                     if cue_dir(kk) == 1 % left
-                        tmp_table.iscued(new_row_idx(1+((kk-1)*2))) = 1;
-                        tmp_table.iscued(new_row_idx(2+((kk-1)*2))) = 0;
+                        tmp_table.is_cued(new_row_idx(1+((kk-1)*2))) = 1;
+                        tmp_table.is_cued(new_row_idx(2+((kk-1)*2))) = 0;
                     elseif cue_dir(kk) == 2 % right
-                        tmp_table.iscued(new_row_idx(1+((kk-1)*2))) = 0;
-                        tmp_table.iscued(new_row_idx(2+((kk-1)*2))) = 1;
+                        tmp_table.is_cued(new_row_idx(1+((kk-1)*2))) = 0;
+                        tmp_table.is_cued(new_row_idx(2+((kk-1)*2))) = 1;
                     end
                 end
             end
@@ -75,14 +75,13 @@ elseif isequal(unique(condition_table_in.stimloc),3)
 
     % update catch/loc/etc.
     tmp_table.unique_im_nr(new_row_idx)    = zeros(n_catch_trials,1);
-    tmp_table.iscatch(new_row_idx)         = true(n_catch_trials,1);
+    tmp_table.is_catch(new_row_idx)         = true(n_catch_trials,1);
     tmp_table.stimloc(new_row_idx)         = 3;
-    tmp_table.stimloc_name(new_row_idx)    ={'center'};
+    tmp_table.stimloc_name(new_row_idx)    = {'center'};
     tmp_table.stim_class(new_row_idx)      = condition_table_in.stim_class(end-1);
     tmp_table.stim_class_name(new_row_idx) = condition_table_in.stim_class_name(end-1);
     tmp_table.task_class(new_row_idx)      = condition_table_in.task_class(end-1);
     tmp_table.task_class_name(new_row_idx) = condition_table_in.task_class_name(end-1);
-    tmp_table.iscued(new_row_idx)        = NaN(n_catch_trials,1);
     
     % insert catch trial randomly
     shuffle_trial_idx = randi(size(condition_table_in,1),n_catch_trials);  % get a new trial number
@@ -90,6 +89,7 @@ elseif isequal(unique(condition_table_in.stimloc),3)
     condition_table_in_post = condition_table_in(shuffle_trial_idx:end,:); % e.g., 6-12
     
     condition_table_out = cat(1,condition_table_in_pre,tmp_table,condition_table_in_post);
+
 end
 
 
