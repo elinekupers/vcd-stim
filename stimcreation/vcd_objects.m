44ecbf14177d9ccacd4bcc9aa128838e577eda7d
Eline Kupers <elinekupers@gmail.com>
2025-04-23 19:52:00 -0500

    add relative rotation name, and header info

--- stimcreation/vcd_objects.m
+++ stimcreation/vcd_objects.m
@@ -1,10 +1,9 @@
-function [objects, masks, im_order, info, p] = vcd_objects(p)
-% VCD function to generate object images:
-% 
-%   [objects, masks, im_order, info, p] = vcd_complexobjects(p)
+function [objects, masks, info] = vcd_objects(params)
+% VCD function:
+%   [objects, masks, info] = vcd_objects(params)
 %
 % Purpose:
-%   Load complex object images for VCD experimental display.
+%   Luminance-correct, load, and store object images for VCD experimental display.
 %   Requires two files:
 %    1. a set of individual object image files, where the folder is defined
 %       by p.stim.obj.indivobjfile, e.g.:
@@ -12,36 +11,24 @@ function [objects, masks, im_order, info, p] = vcd_objects(p)
 %    2. A csv file defined in p.stim.obj.infofile, e.g.: 
 %       fullfile(vcd_rootPath,'workspaces/objects_info.csv')
 %   Given that 0, 90, and 180 degrees are right, forwards, and left facing, 
-%   respectively. Filenames with rotations:
+%   respectively. Facing directions of objects in the following files are:
 %       * 1-23 (0-44 deg): face sideways
 %       * 24-46 (46-90 deg): face forward
 %       * 47-68 (92-134 deg): face forward
 %       * 69-91 (136-180 deg): face sideways
-%   This function will store generated object images as a single mat file 
-%   in p.stim.obj.stimfile (e.g.: fullfile(vcd_rootPath,'workspaces','stimuli','objects.mat')
-%   when p.store_params = true;
+%   
+%   NOTE: If params.stim.obj.dres~=1, function rescales images by
+%   converting to double, [0-1] lum range (assume range is 1-255), and
+%   square as images were made with gamma = 2.
+% 
+%   When params.store_params = true, this function will store generated
+%   object images as a single mat file in params.stim.obj.stimfile (e.g.:
+%   fullfile(vcd_rootPath,'workspaces',....
+%   'stimuli',<disp_name>,'objects.mat').
 %
-%   We reorder and select unique object images from all_unique_im.obj:
-%   unique_im loc  super  basic  sub  facing_dir
-%      1     1     1     1     1    56
-%      2     2     1     2     2   124
-%      3     1     1     2     3    56
-%      4     2     2     1     1   124
-%      5     1     2     1     2    56
-%      6     2     2     2     3   124
-%      7     1     2     2     4    56
-%      8     2     3     1     1   124
-%      9     1     3     1     2    56
-%     10     2     3     2     3   124
-%     11     1     3     2     4    56
-%     12     2     3     3     5   124
-%     13     1     3     3     6    56
-%     14     2     4     1     1   124
-%     15     1     4     1     2    56
-%     16     2     4     1     3   124
 %
 % INPUTS:
-%  p            :   struct with stimulus params
+%  params            :   struct with stimulus params
 %
 % OUTPUTS:
 %  objects      :   uint8 images of complex images, dimensions are
@@ -51,83 +38,91 @@ function [objects, masks, im_order, info, p] = vcd_objects(p)
 %                   background. Dimensions are width (pixels) x height
 %                   (pixels) x object's subordinate category x num of
 %                   rotations
-%  im_order     :   table with information about object rotation and unique
-%                   image number
+%  im_order     :   (struct) with fields containing a subset of info about 
+%                   objects in the order they were loaded:
+%                       object_name: {1×80 cell} - same as filename in info
+%                       base_rot: [1×80 double]  - base rotation of core object
+%                       abs_rot: [1×80 double]   - absolute rotation of object (after adding delta)
+%                       rel_rot: [1×80 double]   - rel rotation of object (i.e. the delta)
+%                       unique_im_nr: [1×80 double] - same as unique_im_nr in info
+%                       facing_dir: {1×80 cell}  - same as facing_dir in info
+%                       rel_rot_name: {1×80 cell} - same as rel_rot_name in info
 %  info         :   Loaded csv from workspaces/stimuli/ with png file names
 %                   and object category information. 
-%  p            :   updated params struct
+%              
+%      filename                         unique_im_nr   stim_pos  stim_pos_i  superordinate    superordinate_i     basic      basic_i  subordinate   subordinate_i  rot_abs  rot_rel   facing_dir   is_in_img  rel_rot_name
+%  {'faces_damonwayans_rot31.png'  }        65         {'left' }    1          {'human'}           1           {'facemale'}     1      {'damon'}        1            60       0       {'forward'}    1       {'rightwards'}              
+%      ...
+%  {'places_watertower_rot75.png'  }        430       {'right'}     2         {'place' }           4           {'tower'}        2      {'watertower'}   3            148      8       {'sideways'}   0       {'rightwards'}    
 %
-% Written by Eline Kupers 2024/12
+% Written by Eline Kupers 2024/12, updated 2025/04
 
 %% If you haven't done so yet, first preprocess object images:
 % s_preprocessObjects.m
 
 
-%% load and sort preprocessed images
-d = dir(sprintf('%s*',fullfile(p.stim.obj.infofile)));
+%% Prepare images
+
+% Get preprocessed image folder
+d = dir(sprintf('%s*',fullfile(params.stim.obj.infofile)));
+
+% Read in table with filenames
 info = readtable(fullfile(d(end).folder,d(end).name));
 
 % Define superordinate and basic categories, and number of exemplars per basic category
-mask            = info.unique_im>0;
+idx0            = info.rot_rel == 0;
 subordinate     = info.subordinate;
-base_rot        = info.rot_abs(mask);                % alternating 2 view ± 4 steps spaced 2 deg apart
+base_rot        = info.rot_abs(idx0);  % alternating 2 view ± 4 steps spaced 2 deg apart
 
-rotation_names  = [info.Properties.VariableNames(~cellfun(@isempty, regexp(info.Properties.VariableNames, 'rot_minus*'))), ...
-    info.Properties.VariableNames(~cellfun(@isempty, regexp(info.Properties.VariableNames, 'rot_plus*')))];
+% Define all rotations
+rotations       = [0, params.stim.obj.delta_from_ref];
 
-rotations = 0;
-for ii = 1:length(rotation_names)
-    rotations = [rotations, choose(regexp(rotation_names{ii}, 'rot_minus'),-1,1).*str2num(cell2mat(regexp(rotation_names{ii}, '\d+','match')))];
-end
-
-rotation_names2 = catcell(2,{{'none'}, rotation_names});
-
-% Preallocate info table
-n_cols   = length(base_rot)*length(rotations);
-im_order = table(NaN(n_cols,1),cell(n_cols,1),NaN(n_cols,1),NaN(n_cols,1),cell(n_cols,1),NaN(n_cols,1),cell(n_cols,1),cell(n_cols,1));
-im_order.Properties.VariableNames = {'unique_im_nr','object_name','abs_rot','rel_rot','rot_name','base_rot','facing_dir','rel_rot_name'};
+% reshape unique image nr for WM test images
+unique_ref_im   = reshape(params.stim.obj.unique_im_nrs_WM, [],sum(idx0));
 
 % Get (rescaled) image extent
-extent   = p.stim.obj.img_sz_pix.*p.stim.obj.dres;
+extent   = params.stim.obj.img_sz_pix.*params.stim.obj.dres;
 
 % Preallocate space
-objects = uint8(ones(extent, extent,3,...
-    length(subordinate),length(rotations)));
-masks = uint8(ones(extent, extent, ...
-    length(subordinate),length(rotations)));
+objects = uint8(ones(extent, extent,3, length(subordinate(idx0)),length(rotations)));
+masks = uint8(ones(extent, extent, length(subordinate(idx0)),length(rotations)));
 
 counter = 1; % we use the lazy counter way to keep track of image
 
-for sub = 1:length(subordinate)
+% Loop over each image
+for sub = 1:length(subordinate(idx0))
     
+    % loop over each rotation
     for rr = 1:length(rotations)
         
         % Get file name
         rot = 1+((base_rot(sub) + rotations(rr))/2);
         fname = sprintf('*%s*_rot%02d.png', subordinate{sub},rot);
-        d = dir(fullfile(p.stim.obj.indivobjfile,fname));
+        d = dir(fullfile(params.stim.obj.indivobjfile,fname));
         if isempty(d)
             error('[%s]: Can''t find image file!')
         end
+        
         if rr==1
             assert(strcmp(info.filename{sub},d.name))
         else
-            assert(strcmp(info.(rotation_names2{rr}){sub},d.name));
+            assert(strcmp(cell2mat(info.filename(length(subordinate(idx0)) + (rr-1) + (length(params.stim.obj.delta_from_ref)*(sub-1)))),d.name));
         end
         
         % Load image
         [im0, ~, alpha0] = imread(fullfile(d.folder,d.name),'BackgroundColor','none');
         
+        % If we need to rescale images:
         % convert to double, [0-1] lum range (assume range is 1-255),
         % and square as images were made with gamma = 2.
-        if ~isequal(p.stim.obj.dres,1) && ~isempty(p.stim.obj.dres)
+        if ~isequal(params.stim.obj.dres,1) && ~isempty(params.stim.obj.dres)
             fprintf('[%s]: Resampling the stimuli..',mfilename);
             
             im0     = (double(im0-1)./254).^2; % convert to [0-1] and square
             alpha0  = (double(alpha0./255)).^2; % convert to [0-1] and square
             
-            im1     = imresize(im0, p.stim.obj.dres); % scale if needed
-            alpha1  = imresize(alpha0,p.stim.obj.dres); % scale if needed
+            im1     = imresize(im0, params.stim.obj.dres); % scale if needed
+            alpha1  = imresize(alpha0,params.stim.obj.dres); % scale if needed
             
             im = uint8(1+(sqrt(im1)*254));
             alpha_im = uint8((sqrt(alpha1)*255));
@@ -137,7 +132,8 @@ for sub = 1:length(subordinate)
         end
         clear im0 alpha0
         
-        %             figure(99); clf; imagesc(im,'AlphaData',alpha_im); colormap gray; colorbar; axis image
+        % in case we want to debug:
+        % figure(99); clf; imagesc(im,'AlphaData',alpha_im); colormap gray; colorbar; axis image
         
         objects(:,:,:,sub,rr) = repmat(im, [1 1 3]);
         masks(:,:,sub,rr) = alpha_im;
@@ -147,8 +143,11 @@ for sub = 1:length(subordinate)
         im_order.base_rot(counter)       = base_rot(sub);
         im_order.abs_rot(counter)        = base_rot(sub)+rotations(rr);
         im_order.rel_rot(counter)        = rotations(rr);
-        im_order.rot_name(counter)       = rotation_names2(rr);
-        im_order.unique_im_nr(counter)   = sub;
+        if rr == 1
+            im_order.unique_im_nr(counter)   = params.stim.obj.unique_im_nrs(sub);
+        else
+            im_order.unique_im_nr(counter)   = unique_ref_im(rr-1,sub);
+        end
         
         % check facing direction        
         if (im_order.abs_rot(counter) < 45) || (im_order.abs_rot(counter) > 135)
@@ -158,6 +157,7 @@ for sub = 1:length(subordinate)
         end
         im_order.facing_dir(counter)     = facing_dir;
         
+        % we define rotations relative to the reference object rotation
         if (im_order.abs_rot(counter)-90) < 0 && im_order.rel_rot(counter) < 0
             im_order.rel_rot_name(counter) = {'rightward'};
         elseif (im_order.abs_rot(counter)-90) < 0 && im_order.rel_rot(counter) > 0
@@ -177,16 +177,17 @@ for sub = 1:length(subordinate)
         elseif im_order.rel_rot(counter) == 0
             im_order.rel_rot_name(counter) = {'none'};
         end
+        
         % Update counter
         counter = counter+1;
     end
 end
 
-if p.stim.store_imgs
+if params.stim.store_imgs
     fprintf('\nStoring images..')
-    saveDir = fileparts(fullfile(p.stim.obj.stimfile));
+    saveDir = fileparts(fullfile(params.stim.obj.stimfile));
     if ~exist(saveDir,'dir'), mkdir(saveDir); end
-    save(fullfile(sprintf('%s_%s.mat',p.stim.obj.stimfile,datestr(now,30))),'objects','masks','info','im_order','-v7.3');
+    save(fullfile(sprintf('%s_%s.mat',params.stim.obj.stimfile,datestr(now,30))),'objects','masks','info','im_order','-v7.3');
 end
 
 %
