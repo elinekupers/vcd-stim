3a5afac45cce5b0666a9492be6f404918910ee70
Eline Kupers <elinekupers@gmail.com>
2025-04-26 16:37:18 -0500

    update units of target size

--- params/vcd_setEyelinkParams.m
+++ params/vcd_setEyelinkParams.m
@@ -20,9 +20,23 @@ el.calibration_success_beep         = [0 0 0]; % no beep
 el.drift_correction_failed_beep     = [0 0 0]; % no beep
 el.drift_correction_success_beep    = [0 0 0]; % no beep
 el.backgroundcolour                 = backgroundcolour; %same background as experiment
-el.msgfontcolour                    = [0 0 0]; %same text as experimentel
-el.calibrationtargetsize            = 1.5; % pixels ??
-el.calibrationtargetwidth           = .6; % inner area pixels???
-el.calibrationtargetcolor           = [0 0 0]; % black?
+el.msgfontcolour                    = [0 0 0]; % black
+el.calibrationtargetsize            = 2.0;     % size of calibration target as percentage of screen. 7TAS height is 1080, 2% = 22 pixels. (el default is 2.5).
+el.calibrationtargetwidth           = 0.92;    % width of calibration target's border as percentage of screen. 7TAS height is 1080, 0.92% = 8 pixels. (el default is 1)
+el.calibrationtargetcolor           = [0 0 0]; % black
+
+% What about these parameters mentioned by Experiment Builder Manual???
+% .outerSize -- Target Outer Size (Integer): The standard calibration and
+% drift correction target is a filled circle (for peripheral detectability)
+% with a central "hole" target (for accurate fixation). The disk is drawn in
+% the calibration foreground color, and the hole is drawn in the calibration
+% background color. The "Target Outer Size" property specifies the diameter
+% of the outer disk of the default calibration target in pixels. This
+% property is only available if "Use Custom Target" is not checked.
+%
+% .innerSize -- Target Inner Size (Integer): Diameter of the inner disk of
+% the default calibration target in pixels). If hole size is 0, no central
+% feature will be drawn. This property is only available if "Use Custom
+% Target" is not checked.
 
 end
