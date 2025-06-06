afe2943ba487dbae1c4806581c841a1457e6c56a
Eline Kupers <elinekupers@gmail.com>
2025-06-06 14:52:59 -0500

    bug fix nearzero tolerance

--- utils/nearZero.m
+++ utils/nearZero.m
@@ -1,7 +1,7 @@
 function status = nearZero(x,tolerance)
 
-if ~exist('tolerance','var') || isempty(tolerance)
-    tolerance = 1.0^-10;
+if nargin==1 || ~exist('tolerance','var') || isempty(tolerance)
+    tolerance = 10^-10;
 end
 
 status = abs(x) < tolerance;
