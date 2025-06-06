16b8fd9721dd1084f13a9ea95f0f9a0f17f15443
Eline Kupers <elinekupers@gmail.com>
2025-06-06 17:27:40 -0500

    fix check for stim and exp params

--- bookkeeping/vcd_createSessions.m
+++ bookkeeping/vcd_createSessions.m
@@ -12,7 +12,9 @@ p0.addRequired('params'           , @isstruct);
 p0.addParameter('condition_master', [], @istable);
 p0.addParameter('load_params'     , true, @islogical);
 p0.addParameter('store_params'    , true, @islogical);
-p0.addParameter('session_env'    , 'MRI', @(x) any(strcmp(x,{'BEHAVIOR','MRI'})));
+p0.addParameter('session_env'     , 'MRI', @(x) any(strcmp(x,{'BEHAVIOR','MRI'})));
+p0.addParameter('randomization_file_pth', [], @ischar);
+p0.addParameter('prefix'          , 'subjXXX_', @ischar);
 
 % Parse inputs
 p0.parse(params,varargin{:});
@@ -69,7 +71,7 @@ else
         end
     end
     
-    % check if session struct is already defined and load it if needed
+    % check if experimental parameters are already defined and load it if needed
     if ~isfield(params,'exp') || isempty(params.exp)
         
         % load trial info
@@ -83,13 +85,38 @@ else
             load(fullfile(d(end).folder,d(end).name),'exp');
             params.exp = exp;
         else
-            error('[%s]: Can''t find exp session .mat files! Please check or run vcd_getSessionParams.m', mfilename);
+            warning('[%s]: Can''t find exp session .mat files! Will run vcd_getSessionParams.m', mfilename);
+            params.exp  = vcd_getSessionParams('load_params',false,'store_params',false, 'verbose',false);
         end
     end
     
-    %% 1. We randomize the blocks and runs within a session and store a file that records this order
-    [condition_master_shuffled, run_matrix,trial_order_local_shuffled, trial_order_global_shuffled] = vcd_randomizeBlocksAndRunsWithinSession(params, condition_master, session_env);
+    % check if stimulus parameters are already defined and load it if needed
+    if ~isfield(params,'stim') || isempty(params.stim)
+        
+        % load trial info
+        d = dir(fullfile(vcd_rootPath,'workspaces','info','stim*.mat'));
+        
+        if ~isempty(d)
+            if length(d) > 1
+                warning('[%s]: Multiple trial.mat files! Will pick the most recent one', mfilename);
+            end
+            load(fullfile(d(end).folder,d(end).name),'stim');
+            params.stim = stim;
+        else
+            warning('[%s]: Can''t find stim session .mat files! Will run vcd_getStimParams.m', mfilename);
+            params.stim = vcd_getStimParams('load_params',false,'store_params',false, 'verbose',false);        
+        end
+    end
     
+    if ~exist('randomization_file_pth','var') || isempty(randomization_file_pth)
+        
+        %% 1. We randomize the blocks and runs within a session and store a file that records this order
+        [condition_master_shuffled, ~, ~, ~] = ...
+            vcd_randomizeBlocksAndRunsWithinSession(params, condition_master, session_env, 'prefix', prefix);
+    else
+        % load provided randomized condition master file
+        load(fullfile(randomization_file_pth,sprintf('%scondition_master_shuffled_%s_%s_*.mat',prefix, params.disp.name, session_env)),'condition_master_shuffled')
+    end
     
     %% 2. We expand the condition master table and add all trial events 
     % (in units of presentationrate_hz frames), and we now call it
