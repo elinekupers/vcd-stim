0997c7dc0d6944de74587fc380bc1397f4f1976f
Eline Kupers <elinekupers@gmail.com>
2025-06-10 16:17:52 -0500

    bug fix concatenating nan_table for older matlab versions

--- bookkeeping/vcd_preallocateNaNTable.m
+++ bookkeeping/vcd_preallocateNaNTable.m
@@ -3,7 +3,7 @@ function nan_table = vcd_preallocateNaNTable(nr_rows, nr_cols, table_example, co
 % copy condition order table headers and scrub content
 sz = [nr_rows nr_cols];
 varTypes = varfun(@class,table_example,'OutputFormat','cell');
-
+varNames = table_example.Properties.VariableNames;
 % check matlab version for backwards compatibility
 % if this version isn't from the 2000s, we need to do make the table the annoying way
 if verLessThan('matlab', '9.6')
@@ -11,15 +11,16 @@ if verLessThan('matlab', '9.6')
     nan_table = table();
     for ii = 1:length(varTypes)
         if strcmp(varTypes{ii},'cell')
-            tmp = table(mat2cell(NaN(sz(1),1), ones(sz(1),1)), 'VariableNames', table_example.Properties.VariableNames(ii));
+            tmp = table(mat2cell(NaN(sz(1),1), ones(sz(1),1)), 'VariableNames', varNames(ii));
             
         elseif strcmp(varTypes{ii},'double')
-            tmp = table(NaN(sz(1),1), 'VariableNames', table_example.Properties.VariableNames(ii));
+            tmp = table(NaN(sz(1),1), 'VariableNames', varNames(ii));
         end
+        nan_table = cat(2, nan_table, tmp);
     end
-    nan_table = [nan_table; tmp];
+    
 else
-    nan_table = table('Size',sz,'VariableTypes',varTypes, 'VariableNames',table_example.Properties.VariableNames);
+    nan_table = table('Size',sz,'VariableTypes',varTypes, 'VariableNames',varNames);
 end
 
 for vt = 1:length(varTypes)
