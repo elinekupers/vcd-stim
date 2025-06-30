b4a69c08e5afb765fed34e343ec77e60346ebeb2
Eline Kupers <elinekupers@gmail.com>
2025-06-30 18:45:58 -0500

    add logical option, throw an error if none of the variable class types match

--- bookkeeping/vcd_preallocateNaNTable.m
+++ bookkeeping/vcd_preallocateNaNTable.m
@@ -12,14 +12,19 @@ if verLessThan('matlab', '9.6')
     for ii = 1:length(varTypes)
         if strcmp(varTypes{ii},'cell')
             tmp = table(mat2cell(NaN(sz(1),1), ones(sz(1),1)), 'VariableNames', varNames(ii));
-            
+        elseif strcmp(varTypes{ii},'logical')
+            tmp = table(false(sz(1),1), 'VariableNames', varNames(ii));
         elseif strcmp(varTypes{ii},'double')
             tmp = table(NaN(sz(1),1), 'VariableNames', varNames(ii));
+        else
+            error('[%s]: Not sure what to do with class type of %s.',mfilename,varTypes{ii})
         end
         nan_table = cat(2, nan_table, tmp);
     end
     
 else
+    % Otherwise we start the way we intend to, creating a "default" table 
+    % (zeros for all rows) and update class type below. 
     nan_table = table('Size',sz,'VariableTypes',varTypes, 'VariableNames',varNames);
 end
 
@@ -37,12 +42,17 @@ for vt = 1:length(varTypes)
     if strcmp(varTypes(vt),'double')
         nan_table.(table_example.Properties.VariableNames{vt}) = NaN(sz(1),cw);
         
+    elseif strcmp(varTypes(vt),'logical')
+        nan_table.(table_example.Properties.VariableNames{vt}) = false(sz(1),cw);
+        
     elseif strcmp(varTypes(vt),'cell')
         if isequalwithequalnans(table2cell(table_example(1,vt)), {NaN(1,cw)})
             nan_table.(table_example.Properties.VariableNames{vt}) = repmat({NaN(1,cw)},sz(1),1);
         else
             nan_table.(table_example.Properties.VariableNames{vt}) = cell(sz(1),cw);
         end
+    else
+        error('[%s]: Not sure what to do with class type of %s.',mfilename,varTypes{vt})
     end
     
 end
