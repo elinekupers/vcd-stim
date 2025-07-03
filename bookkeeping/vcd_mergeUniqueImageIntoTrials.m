541465984c32193dc337f02ba8b239e353557d63
Eline Kupers <elinekupers@gmail.com>
2025-07-02 20:03:32 -0500

    check if is_special_core is 1 and is_catch is 0, not that  they are not logical vectors anymore

--- bookkeeping/vcd_mergeUniqueImageIntoTrials.m
+++ bookkeeping/vcd_mergeUniqueImageIntoTrials.m
@@ -63,10 +63,15 @@ if ~strcmp(unique(conds_master.task_class_name),{'fix'})
     if ismember(unique(conds_master.task_class_name),{'img','ltm'})  
 
         % double the nr of trials for imagery, but not catch trials, given that we only selected a subset
-        left_cued    = [shuffle_concat(left_cued(ismember(left_cued,find(conds_master.is_special_core)))', 2)'; left_cued(ismember(left_cued,find(conds_master.is_catch)))];
-        left_uncued  = [shuffle_concat(left_uncued(ismember(left_uncued,find(conds_master.is_special_core)))', 2)'; left_uncued(ismember(left_uncued,find(conds_master.is_catch)))];
-        right_cued   = [shuffle_concat(right_cued(ismember(right_cued,find(conds_master.is_special_core)))', 2)'; right_cued(ismember(right_cued,find(conds_master.is_catch)))];
-        right_uncued = [shuffle_concat(right_uncued(ismember(right_uncued,find(conds_master.is_special_core)))', 2)';right_uncued(ismember(right_uncued,find(conds_master.is_catch)))];
+        non_catch_left_cued    = left_cued(ismember(left_cued,find(conds_master.is_special_core==1 & conds_master.is_catch==0)));
+        non_catch_left_uncued  = left_uncued(ismember(left_uncued,find(conds_master.is_special_core==1 & conds_master.is_catch==0)));
+        non_catch_right_cued   = right_cued(ismember(right_cued,find(conds_master.is_special_core==1 & conds_master.is_catch==0)));
+        non_catch_right_uncued = right_uncued(ismember(right_uncued,find(conds_master.is_special_core==1 & conds_master.is_catch==0)));
+        
+        left_cued    = [shuffle_concat(non_catch_left_cued', 2)';    left_cued(ismember(left_cued,find(conds_master.is_catch==1)))];
+        left_uncued  = [shuffle_concat(non_catch_left_uncued', 2)';  left_uncued(ismember(left_uncued,find(conds_master.is_catch==1)))];
+        right_cued   = [shuffle_concat(non_catch_right_cued', 2)';   right_cued(ismember(right_cued,find(conds_master.is_catch==1)))];
+        right_uncued = [shuffle_concat(non_catch_right_uncued', 2)'; right_uncued(ismember(right_uncued,find(conds_master.is_catch==1)))];
 
         % update trial vec and trial vec idx 
         trial_vec_i = NaN(length(left_cued)*2,2);
