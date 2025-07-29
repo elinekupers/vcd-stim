1fa3f72c0ad1d90f2a3b059bfdb8a9824c76db07
Eline Kupers <elinekupers@gmail.com>
2025-07-29 11:14:04 -0700

    make ltm stim pairs bidirectional, and exclude non-special core stimuli in the pairings

--- bookkeeping/vcd_pairUniqueImagesLTM.m
+++ bookkeeping/vcd_pairUniqueImagesLTM.m
@@ -8,11 +8,13 @@ function ltmPairs = vcd_pairUniqueImagesLTM(params)
 %
 % This function will take the 47 special core VCD stimuli for each stimulus
 % class (what we call image A), and pair them to a non-special core
-% stimulus (image B).
+% stimulus (image B). Special core stimuli are used to create 23 pairs:
+% * 32 classic special core stimuli are involved in 16 pairs amongst themselves
+% * 15 NS special core stimuli are involved in 7 pairs amongst themselves (I guess leave one out??)
 %
-% Constraints: 
+% Pairing constraints: 
 % * A and B pairings are unique. We will not reuse A's or B's.
-% * Pairing is directional A --> B.
+% * Pairing is bidirectional A --> B and B --> A
 % > Classic stimuli:
 %   * A's and B's cannot come from the same stimulus class.
 %   * Maximize stimulus class variability, such that A's from one stimulus
@@ -33,20 +35,20 @@ function ltmPairs = vcd_pairUniqueImagesLTM(params)
 % 
 % OUTPUT:
 % * ltmPairs    : (double) 47 x 2 matrix where the first column is the
-%                 special core unique image number for A. The second column 
-%                 is the associated stimulus paired with A (non-special 
-%                 core unique image number for B) 
+%                 unique special core stimulus number for A. The second 
+%                 column is the unique special core stimulus number for B.
 %
 % Example:
-% params.stim = vcd_getStimParams('load_params',false,'store_params',false, 'verbose',false);
-% params.exp = vcd_getSessionParams('load_params',false,'store_params',false, 'verbose',false);
+% params.disp = vcd_getDisplayParams('7TAS_BOLDSCREEN32');
+% params.exp  = vcd_getSessionParams('disp_name','7TAS_BOLDSCREEN32');
+% params.stim = vcd_getStimParams('disp_name','7TAS_BOLDSCREEN32');
 % ltmPairs = vcd_pairUniqueImagesLTM(params)
 %
-% Written by Eline K. @ UMN 2025/05
+% Written by Eline K. @ UMN 2025/05, update 2025/07
 
-% Preallocate space for A's and B's 
-A = [];
-B = [];
+% Preallocate space for special_core and non_special_core stimuli
+special_core    = [];
+nonspecial_core = [];
 
 for ii = 1:length(params.exp.stimclassnames)
     
@@ -55,117 +57,113 @@ for ii = 1:length(params.exp.stimclassnames)
 
     csv = readtable(fullfile(d(end).folder,d(end).name));
 
-    [special_core_idx]      = ismember(csv.unique_im, params.stim.(params.exp.stimclassnames{ii}).unique_im_nrs_specialcore);
-    non_special_core_num = setdiff(params.stim.(params.exp.stimclassnames{ii}).unique_im_nrs_core,params.stim.(params.exp.stimclassnames{ii}).unique_im_nrs_specialcore);
-    [non_special_core_idx]  = ismember(csv.unique_im, non_special_core_num);
+    special_core_idx      = ismember(csv.unique_im, params.stim.(params.exp.stimclassnames{ii}).unique_im_nrs_specialcore);
+    non_special_core_num  = setdiff(params.stim.(params.exp.stimclassnames{ii}).unique_im_nrs_core,params.stim.(params.exp.stimclassnames{ii}).unique_im_nrs_specialcore);
+    non_special_core_idx  = ismember(csv.unique_im, non_special_core_num);
     
-    special_core_nums = csv.unique_im(special_core_idx);
-    nonspecial_core_nums = csv.unique_im(non_special_core_idx);
+    special_core_nums       = csv.unique_im(special_core_idx);
+    nonspecial_core_nums    = csv.unique_im(non_special_core_idx);
 
-    A = cat(1,A, [sort(special_core_nums), ii.*ones(length(special_core_nums),1)]);
-    B = cat(1,B, [sort(nonspecial_core_nums), ii.*ones(length(nonspecial_core_nums),1)]);
+    special_core    = cat(1,special_core, [sort(special_core_nums), ii.*ones(length(special_core_nums),1)]);
+    nonspecial_core = cat(1,nonspecial_core, [sort(nonspecial_core_nums), ii.*ones(length(nonspecial_core_nums),1)]);
 
     if ii == 5
-        A_cat_info = [csv.superordinate_i(special_core_idx),csv.basic_i(special_core_idx)];
-        A_cat_name = csv.exemplar(special_core_idx);
-        B_cat_info = [csv.superordinate_i(non_special_core_idx),csv.basic_i(non_special_core_idx)];
-        B_cat_name = csv.exemplar(non_special_core_idx);
+        special_core_cat_info    = [csv.super_cat(special_core_idx),csv.basic_cat(special_core_idx)];
     end
 end
-assert(sum(ismember(A(:,1),B(:,1),'rows'))==0) % A's cannot be B's 
-assert(sum(ismember(B(:,1),A(:,1),'rows'))==0) % B's cannot be A's 
+assert(sum(ismember(special_core(:,1),nonspecial_core(:,1),'rows'))==0) % special_core's cannot be nonspecial_core's 
+assert(sum(ismember(nonspecial_core(:,1),special_core(:,1),'rows'))==0) % special_core's cannot be nonspecial_core's 
 
 %% Assign pairs for classic stimuli
-B_no_scenes = B(B(:,2)~=5,:); % grab the non scene B's
-B_yes_scenes = B(B(:,2)==5,:); % let's also define the scene B's
+special_core_no_scenes = special_core(special_core(:,2)~=5,:); % grab the non scene special_core's
+special_core_yes_scenes = special_core(special_core(:,2)==5,:); % let's also define the scene special_core's
 
-% Get the stimulus class
-unique_B_no_scenes = unique(B_no_scenes(:,2))';
+pairnums_classic = []; pairclass_classic = [];
 
-
-pairnums_classic = []; sample_check = 1;
-
-for jj = unique_B_no_scenes
-    % given the current stim class, find the stimuli that are not part 
-    % of that stimulus class
-    nonoverlapping_Bs = B_no_scenes(B_no_scenes(:,2)~=jj,1);
+while 1
+    % Generate shuffled image order
+    shuffle_idx = randperm(length(special_core_no_scenes),length(special_core_no_scenes));
     
-    % See how many pairs we need to generate
-    n_pairs = sum(A(:,2)==jj);
+    % Apply shuffle
+    shuffled_im = special_core_no_scenes(shuffle_idx,:);
     
-    while sample_check
-        % sample the B's
-        sampled_Bs = datasample(nonoverlapping_Bs, n_pairs,'Replace',false); % randomly select from available B pool without replacement
+    % Split shuffled list of classic special core stimuli in A's and B's
+    A = shuffled_im(1:(length(shuffled_im)/2),:);
+    B = shuffled_im((1+(length(shuffled_im)/2)):end,:);
+    
+    % Check now many samples we have for each stimulus class
+    count_stimclassA = histcounts(A(:,2));
+    count_stimclassB = histcounts(B(:,2));
+    
+    overlap_stimclassAB = (A(:,2) == B(:,2));
         
-        % Store them temporarily
-        tmp_pairnum = B_no_scenes(ismember(B_no_scenes(:,1),sampled_Bs), :);
-        
-        % Check now many samples we have for each stimulus class
-        count_stimclass = histcounts(tmp_pairnum(:,2));
-        
-        % If we have more than 2 per class, we break the while loop
-        if (length(count_stimclass(count_stimclass~=0))==3) && all(count_stimclass(count_stimclass~=0)>=2)
-            B_no_scenes(ismember(B_no_scenes(:,1),sampled_Bs),:) = []; % remove the B's we used
-            pairnums_classic = cat(1,pairnums_classic,tmp_pairnum); % we add the pairs
-            break % we break the while loop 
-        end
+    % If we have more than 2 per class, we break the while loop
+    if all(count_stimclassA==4) && all(count_stimclassB==4) ...
+        && sum(overlap_stimclassAB)==0
+        % combine the pairs
+        pairnums_classic  = [A(:,1), B(:,1)]; 
+        pairclass_classic = [A(:,2), B(:,2)];
+        break
     end
+    
 end
 
+assert(sum(ismember(A(:,1), B(:,1)))==0)
+assert(sum(ismember(B(:,1), A(:,1)))==0)
+assert(all(A(:,2)~=B(:,2)))
+
 %% Now do the same for NS
-pairnums_ns = []; sample_check = 1;
+pairnums_ns = []; 
 
-% Get the B scenes, add super and basix category information
-unique_super_cats = unique(B_cat_info(:,1))';
-B_scenes = cat(2,B_yes_scenes(:,1),B_cat_info);
+% Get scenes, add super and basix category information
+specialcore_scenes = cat(2,special_core_yes_scenes(:,1),special_core_cat_info);
 
-% Get the A scenes, add super and basix category information
-A_scenes = A(A(:,2)==5);
-A_scenes = cat(2,A_scenes,A_cat_info);
-
-for jj = unique_super_cats
-    % given the current super category, find the scenes that are not part 
-    % of that super category
-    nonoverlapping_Bs = B_scenes(B_scenes(:,2)~=jj,1);
+while 1
     
-    % See how many pairs we need to generate
-    n_pairs = sum(A_scenes(:,2)==jj);
+    % Generate shuffled image order
+    shuffle_idx = randperm(length(specialcore_scenes),length(specialcore_scenes));
     
-    while sample_check
-        % sample the B's
-        sampled_Bs = datasample(nonoverlapping_Bs, n_pairs,'Replace',false); % randomly select from available B pool without replacement
-        
-        % Store them temporarily
-        tmp_pairnum = B_scenes(ismember(B_scenes(:,1),sampled_Bs), :);
-        
-        % Check now many samples we have for each super category
-        count_stimclass = histcounts(tmp_pairnum(:,2));
-        
-        % given that there are exactly 15 As and 15 Bs, we assume that for
-        % the last supercategory, the scenes will be from the non-sampled 
-        % super category.
-        if jj == unique_super_cats
-            pairnums_ns = cat(1,pairnums_ns,tmp_pairnum); % we add the pairs
-            break;
-        % If we have 2 or more Bs per super category, 
-        elseif (length(count_stimclass(count_stimclass~=0))>=2)
-            B_scenes(ismember(B_scenes(:,1),sampled_Bs),:) = []; % remove the B's we used
-            pairnums_ns = cat(1,pairnums_ns,tmp_pairnum); % we add the pairs
-            break % we break the while loop 
-        end
+    % Apply shuffle
+    shuffled_im = specialcore_scenes(shuffle_idx,:);
+    
+    % Split shuffled list of classic special core stimuli in A's and B's
+    A = shuffled_im(1:(length(shuffled_im)/2),:);
+    B = shuffled_im((1+(length(shuffled_im)/2)):end,:);
+    
+    % Check now many samples we have for each stimulus class
+    count_stimclassA = histcounts(A(:,2),[1:6]);
+    count_stimclassB = histcounts(B(:,2),[1:6]);
+    
+    overlap_stimclassAB = (A(:,2) == B(:,2));
+    
+    % If we have more than 2 per class, we break the while loop
+    if all(count_stimclassA>=1) && all(count_stimclassB>=1) ...
+            && sum(overlap_stimclassAB)==0
+        % combine the pairs
+        pairnums_ns     = [A(:,1), B(:,1)];
+        pairsupercat_ns = [A(:,2), B(:,2)];
+        break
     end
 end
 
 % Concatenate classic and ns stim pairings.
-ltmPairs_w_info = cat(2, A, cat(1,pairnums_classic,cat(2,pairnums_ns(:,1),B_yes_scenes(:,2))));
 
-% Only select relevant columns (unique image nrs for A and B)
-ltmPairs = ltmPairs_w_info(:,[1,3]);
+% unique image nrs for A and B (note: we duplicate B's as A's, as pairs are bidirectional)
+ltmPairs = cat(1, pairnums_classic, [pairnums_classic(:,2),pairnums_classic(:,1)], ...
+                  pairnums_ns, [pairnums_ns(:,2),pairnums_ns(:,1)]);           
+ltmPairs_stimclass = cat(1, pairclass_classic, [pairclass_classic(:,2),pairclass_classic(:,1)], ...
+                  5*ones(size(pairnums_ns)), 5*ones(size(pairnums_ns)));
+% sort order
+[~,sort_idx]       = sort(ltmPairs(:,1));        
+ltmPairs           = ltmPairs(sort_idx,:);
+ltmPairs_stimclass = ltmPairs_stimclass(sort_idx,:);
+
+% concatenate stim nrs and stim class
+ltmPairs_w_info = cat(2,ltmPairs, ltmPairs_stimclass);
 
 % Do some checks
 assert( all(ltmPairs_w_info((ltmPairs_w_info(:,2)<5),2) ~= ltmPairs_w_info((ltmPairs_w_info(:,2)<5),4))); % classic A's and B's cannot be from the same stim class
 assert( all(ltmPairs_w_info(:,1) ~= ltmPairs_w_info(:,3))); % A's cannot be matched with themselves
 assert( all(ltmPairs_w_info((ltmPairs_w_info(:,2)==5),1) ~= ltmPairs_w_info((ltmPairs_w_info(:,2)==5),3))); % ns A's cannot be ns B's
-assert( all(A_scenes(:,2) ~= pairnums_ns(:,2))); % supercat of ns A's cannot be supercat of ns B's
-assert( all(~ismember(ltmPairs_w_info(:,1),ltmPairs_w_info(:,2)))); % A's cannot be B's, regardless of paring (they are non overlapping).
+assert( all(pairsupercat_ns(:,1) ~= pairsupercat_ns(:,2))); % supercat of ns A's cannot be supercat of ns B's
 
