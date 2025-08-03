a91fb2d3c527c96a800dc857244e41517b82c3ad
Eline Kupers <elinekupers@gmail.com>
2025-08-03 15:19:02 -0700

    add 7T stim test

--- tinkers/ek_testbed.m
+++ tinkers/ek_testbed.m
@@ -38,7 +38,7 @@ data_dir = fullfile(vcd_rootPath, 'data','BEHAVIOR',sprintf('vcd_subj%03d',subj_
 
 all_images = struct;
 [data,all_images] = runme_vcdcore(subj_nr, ses_nr, 1, 1, 'PPROOM_EIZOFLEXSCAN', 'wanteyetracking', false, ...
-'all_images',all_images,'wantdatabypass',true,'exp_env',4); 
+'all_images',all_images,'wantdatabypass',false,'wantptbbypass',true,'exp_env',4); 
 
 tt_file    = dir(fullfile(data_dir, sprintf('vcd_subj%03d_time_table_master_PPROOM_EIZOFLEXSCAN_*.mat',subj_nr)));
 load(fullfile(tt_file(end).folder, tt_file(end).name));
@@ -87,3 +87,45 @@ tt_file    = dir(fullfile(vcd_rootPath,'data','BEHAVIOR',sprintf('vcd_subj%03d',
     sprintf('vcd_subj%03d_time_table_master_PPROOM_EIZOFLEXSCAN_*.mat',subj_nr)));
 load(fullfile(tt_file(end).folder, tt_file(end).name));
 vcd_checkTimeTable(time_table_master,behresults)
+
+
+%% DRY-RUN 10 RUNS OF WIDE MRI EXPERIMENT
+subj_nr = 000;
+ses_nr = 1;
+ses_type = [1,2];
+
+data_dir = fullfile(vcd_rootPath, 'data','MRI',sprintf('vcd_subj%03d',subj_nr));
+
+for st = ses_type
+    all_images = struct;
+    [data,all_images] = runme_vcdcore(subj_nr, ses_nr, st, 1, '7TAS_BOLDSCREEN32', 'wanteyetracking', false, ...
+        'all_images',all_images,'wantdatabypass',true,'exp_env',4);
+    
+    tt_file    = dir(fullfile(data_dir, sprintf('vcd_subj%03d_time_table_master_wide_7TAS_BOLDSCREEN32_*.mat',subj_nr)));
+    load(fullfile(tt_file(end).folder, tt_file(end).name));
+    writetable(time_table_master,'~/Desktop/time_table_master_wide.csv');
+    writetable(all_run_frames,'~/Desktop/all_run_frames_wide.csv');
+    
+    all_images = struct;
+    for rr = 1:10
+        [data,all_images] = runme_vcdcore(subj_nr, ses_nr, 1, rr, '7TAS_BOLDSCREEN32', 'wanteyetracking', false, ...
+            'all_images',all_images,'wantdatabypass',true,'exp_env',4, 'timetable_file',fullfile(tt_file(end).folder, tt_file(end).name));
+    end
+end
+
+for st = ses_type
+    clear behresults
+    for rr = 1:10
+        subj_folder = sprintf('vcd_subj%03d_ses%02d',subj_nr,ses_nr);
+        mat_file    = sprintf('behavior_*_vcd_subj%03d_ses%02d_A_run%02d.mat',subj_nr,ses_nr,rr);
+        dd = dir(fullfile(vcd_rootPath,'data','MRI',subj_folder,mat_file));
+        load(fullfile(dd(end).folder,dd(end).name));
+        performance = vcdbehavioralanalysis(fullfile(params.savedatafolder,params.behaviorfile));
+        
+        writetable(run_frames,sprintf('~/Desktop/run_frames%02d_wide.csv',rr));
+        writetable(run_table,sprintf('~/Desktop/run_table%02d_wide.csv',rr));
+        behresults(rr) = performance;
+    end
+    
+    vcd_checkTimeTable(time_table_master,behresults)
+end
