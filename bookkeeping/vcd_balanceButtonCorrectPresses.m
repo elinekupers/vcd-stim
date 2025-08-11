1d3ae01b3fb131e97e4c06a836302ae4aa1bd53c
Eline Kupers <elinekupers@gmail.com>
2025-08-11 15:08:56 -0700

    add comments

--- bookkeeping/vcd_balanceButtonCorrectPresses.m
+++ bookkeeping/vcd_balanceButtonCorrectPresses.m
@@ -1,5 +1,5 @@
 function condition_master = vcd_balanceButtonCorrectPresses(params, condition_master, env_type)
-% VCD bookkeeping function to check if button presses are balanced across 
+% VCD bookkeeping function to check if button presses are balanced across
 % blocks and comply with our constraints. If not, then this function will
 % reorder trials until they do comply.
 %
@@ -11,9 +11,9 @@ function condition_master = vcd_balanceButtonCorrectPresses(params, condition_ma
 % Enforced constraints:
 % 1. Each block within a session (and session type) but have 50/50
 %    left/right cueing for classic stimuli.
-% 2. We allow for max 2 repeats in a single-image block (which has 4 trials), 
+% 2. We allow for max 2 repeats in a single-image block (which has 4 trials),
 %    and 1 repeat within a double-image block (which has 8 trials)
-% 3. Each block within a session must have equal distribution of correct 
+% 3. Each block within a session must have equal distribution of correct
 %    button presses, except for:
 %       * WHAT/HOW tasks -- we go by category info, because we combine
 %       object and foods into one button press.
@@ -26,16 +26,16 @@ function condition_master = vcd_balanceButtonCorrectPresses(params, condition_ma
 % Note that we ignore fixation and CD trials for now, as FIX has
 % pseudotrials and we may have not selected CD+ trials yet (and will check
 % button press distribution separately).
-% 
+%
 % INPUTS:
 %   params                :  (struct) parameter struct needed to get subject
 %                             nrs and run type params. REQUIRES params.trials
 %                             to exist and contain condition_master v0.
 %   condition_master      :  (table) general condition_master table with
-%                             block and runs. If it does not have session 
+%                             block and runs. If it does not have session
 %                             nr or session type columns, then we will
-%                             infer this from "ses_blocks", 
-%                             params.exp.session.mri.wide.ses_blocks, or 
+%                             infer this from "ses_blocks",
+%                             params.exp.session.mri.wide.ses_blocks, or
 %                             params.exp.session.mri.deep.ses_blocks, or
 %                             params.exp.session.behavior.ses_blocks.
 %   env_type              :  (str) label to define what type of session we
@@ -72,13 +72,15 @@ if strcmp(env_type, 'MRI')
 elseif strcmp(env_type, 'BEHAVIOR')
     total_catch_trials = zeros(5,10);
 end
-    
+
 % Reset counters
 all_stimtask_counter = zeros(length(params.exp.stimclassnames),length(params.exp.taskclassnames)); % 5 stim classes x 10 task classes
 
 % Reset flag
 reshuffle_me = false;
 
+% Reset stim_class_unique_block_nr (used by vcd_allocateBlocksToRuns.m) and
+% unique_trial_nr. We will update these numbers after reordering trials.
 condition_master.stim_class_unique_block_nr = NaN(size(condition_master.stim_class_unique_block_nr));
 condition_master.unique_trial_nr = NaN(size(condition_master.unique_trial_nr));
 
@@ -105,9 +107,11 @@ for ses = session_nrs
                         
                         % Allocated blocks and trials
                         nr_catch_trials = total_catch_trials(sc,tc);
-                        
+                        % Get nr of blocks from the params struct
                         nr_blocks = stimtask_counter(sc,tc);
+                        % Add catch trials
                         nr_trials = nr_catch_trials + (params.exp.nr_trials_per_block(sc,tc)*nr_blocks);
+                        % Update nr of blocks after adding catch trials
                         nr_blocks = ceil(nr_trials/params.exp.nr_trials_per_block(sc,tc));
                         
                         if nr_trials > 0
@@ -126,7 +130,7 @@ for ses = session_nrs
                             trial_to_condition_ratio = nr_trials/nr_unique_conds;
                             % If we have any left over trials from session
                             % type 1 (A), then we want to add a few more
-                            % trials as possible optons to avoid getting 
+                            % trials as possible optons to avoid getting
                             % stuck in a local minimum.
                             if nr_catch_trials > 0
                                 if (st == 2) && (trial_to_condition_ratio >= 0.75 && trial_to_condition_ratio < 1)
@@ -139,7 +143,7 @@ for ses = session_nrs
                                     end
                                 end
                             end
-                                
+
                             while 1 % get trials for each stim-task crossing, separate for each session type
                                 st_idx    = false(size(condition_master,1),1);
                                 
@@ -171,9 +175,9 @@ for ses = session_nrs
                                     cued_conds       = condition_master.is_cued(ti(potential_trials(1:nr_trials)));
                                     stim_nr_left     = condition_master.stim_nr_left(ti(potential_trials(1:nr_trials)));
                                     stim_nr_right    = condition_master.stim_nr_right(ti(potential_trials(1:nr_trials)));
-                                    % check if (A) if cueing left/right is balanced within a block. 
+                                    % check if (A) if cueing left/right is balanced within a block.
                                     % (B) if we have any stimulus number
-                                    % repeats within a block (we allow for 2 repeats in a single-image block (which has 4 trials), 
+                                    % repeats within a block (we allow for 2 repeats in a single-image block (which has 4 trials),
                                     % and 1 repeat within a double-image block (which has 8 trials)
                                     if all(cued_conds==3)
                                         stim_nr_repeats = zeros(nr_blocks,1);
@@ -200,6 +204,7 @@ for ses = session_nrs
                                             if diff(cued_counts)==0
                                                 bb_ok(bb) = 1;
                                             end
+                                            % Check if we repeat the same stimulus number, if we have more than two repeats per block then we reshuffle
                                             if (params.exp.nr_trials_per_block(sc,tc) == 4) && (length(stim_nr_left(bbi)) - length(unique(stim_nr_left(bbi))) <= 1)
                                                 stim_nr_repeats(bb,1) = 1;
                                             elseif (params.exp.nr_trials_per_block(sc,tc) == 8) && (length(stim_nr_left(bbi)) - length(unique(stim_nr_left(bbi))) <= 2)
@@ -213,15 +218,16 @@ for ses = session_nrs
                                         end
                                         if (sum(stim_nr_repeats(:))/2) == nr_blocks && sum(bb_ok) == nr_blocks
                                             break;
-%                                         elseif trial_to_condition_ratio == 1 && sum(catch_idx)>0
-%                                             if nr_unique_conds < params.exp.nr_unique_trials_per_crossing(sc,tc)+sum(catch_idx)
-%                                                 nr_unique_conds = nr_unique_conds + 1;
-%                                             end
-%                                             reshuffle_me = true;
+                                            %                                         elseif trial_to_condition_ratio == 1 && sum(catch_idx)>0
+                                            %                                             if nr_unique_conds < params.exp.nr_unique_trials_per_crossing(sc,tc)+sum(catch_idx)
+                                            %                                                 nr_unique_conds = nr_unique_conds + 1;
+                                            %                                             end
+                                            %                                             reshuffle_me = true;
                                         else
                                             reshuffle_me = true;
                                         end
                                     end
+                                    
                                 end
                                 assert(all(st_idx(ti(potential_trials(1:nr_trials)))==0))
                                 st_idx = (ti(potential_trials(1:nr_trials)));
@@ -230,10 +236,10 @@ for ses = session_nrs
                                 curr_tc = tc;
                                 assert(all(ismember(condition_master.stim_class(st_idx), curr_sc)));
                                 assert(all(ismember(condition_master.task_class(st_idx), curr_tc)));
-
+                                
                                 % check if we have such a crossing
                                 if sum(ismember(condition_master.task_class(st_idx), curr_tc) & ismember(condition_master.stim_class(st_idx), curr_sc))>0
-
+                                    
                                     % if so, then set the number of expected button presses
                                     % **** !!! careful this is hardcoded stuff !!! *****
                                     if ismember(curr_tc,[2,4,5,6,7]) % cd, pc, wm, ltm, img have 2 response options
@@ -243,11 +249,11 @@ for ses = session_nrs
                                     elseif ismember(curr_tc,[9]) % where has 3 response options
                                         nr_responses = 3;
                                     end
-
+                                    
                                     resp = condition_master.correct_response(st_idx);
                                     resp = resp(~catch_idx);
                                     n = histcounts(resp,1:(nr_responses+1));
-
+                                    
                                     % For WHAT/HOW tasks we go by category info, because we combine
                                     % object and foods into one button press..
                                     if ismember(curr_sc,[4,5]) && ismember(curr_tc,8) % **** !!! careful this is hardcoded stuff !!! *****
@@ -259,7 +265,7 @@ for ses = session_nrs
                                             n0     = histcounts(supercat,1:6);
                                         end
                                         n1 = [n0([1,2]), n0(3)+n0(4), n0(5)];  % we combine (3) objects and (4) food into a single button press (the ring finger)
-
+                                        
                                         if (all(n==n1))
                                             % all responses are balanced
                                             reshuffle_me = false;
@@ -280,7 +286,7 @@ for ses = session_nrs
                                     elseif ismember(curr_sc,4) && ismember(curr_tc,4) % PC-OBJ
                                         resp2 = resp(~objectcatch_idx); % only check non-obj catch  trials
                                         n2 = histcounts(resp2,1:(nr_responses+1));
-
+                                        
                                         if (diff(n2)==0)
                                             % ideally all responses are balanced
                                             reshuffle_me = false;
@@ -319,7 +325,7 @@ for ses = session_nrs
                                                 end
                                             end
                                         end
-
+                                        
                                     elseif ismember(curr_sc,5) && ismember(curr_tc,9) % NS-WHERE
                                         
                                         if all(diff(n)==0) % we want equal distribution of L/C/R
@@ -351,11 +357,11 @@ for ses = session_nrs
                                             cued0 = condition_master.is_cued(st_idx);
                                             cued0 = cued0(~catch_idx);
                                             n1    = histcounts([affordcat(cued0==1,1);affordcat(cued0==2,2)],1:(nr_responses+1));
-
+                                            
                                         elseif curr_sc == 5 % NS-HOW
                                             n1     = histcounts(affordcat,1:(nr_responses+1));
                                         end
-
+                                        
                                         if (all(n==n1))
                                             reshuffle_me = false;
                                             break % hurray
@@ -371,7 +377,7 @@ for ses = session_nrs
                                         else
                                             reshuffle_me = true;
                                         end
-
+                                        
                                     elseif ismember(curr_sc,99) || ismember(curr_tc,3) % scc-all
                                         % Check if we sample all core images across trials
                                         stmclass0 = condition_master.stim_class_name(st_idx,:);
@@ -380,9 +386,9 @@ for ses = session_nrs
                                         cued0     = cued0(~catch_idx);
                                         stmclass0_cued      = [stmclass0(cued0==1,1);stmclass0(cued0==2,2)];
                                         [~,stmclass_cued_i] = ismember(stmclass0_cued,params.exp.stimclassnames([1,3,2,4]));
-
+                                        
                                         n1 = histcounts(stmclass_cued_i,1:(nr_responses+1));
-
+                                        
                                         if (all(n==n1))
                                             reshuffle_me = false;
                                             break % hurray
@@ -398,16 +404,16 @@ for ses = session_nrs
                                         else
                                             reshuffle_me = true;
                                         end
-
+                                        
                                     elseif all(n==0) && curr_tc~=2 && ~ismember(curr_tc,[8,9,10])
                                         error('[%s]: No correct responses found?!',mfilename);
                                     else
                                         if (all(diff(n)==0)) % check if we have balanced button presses
                                             reshuffle_me = false;
                                             break % hurray
-                                        elseif trial_to_condition_ratio == 1 && sum(catch_idx) > 0 
+                                        elseif trial_to_condition_ratio == 1 && sum(catch_idx) > 0
                                             % if we sampled all unique conditions AND have catch trials,
-                                            % then we will likely never reach a solution unless we allow for a small difference in response distribution.  
+                                            % then we will likely never reach a solution unless we allow for a small difference in response distribution.
                                             if (all(diff(n)<=sum(catch_idx)))
                                                 reshuffle_me = false;
                                                 break;
@@ -417,7 +423,7 @@ for ses = session_nrs
                                         else
                                             reshuffle_me = true;
                                         end
-
+                                        
                                     end
                                 end
                             end
@@ -478,7 +484,7 @@ leftover_trials = (isnan(condition_master.unique_trial_nr));
 
 for curr_sc = 1:(length(unique_stim_classes)-1)
     for curr_tc = 1:length(unique_task_classes)
-        if unique_task_classes(curr_tc) == 3 && unique_stim_classes(curr_sc) < 5 % SCC 
+        if unique_task_classes(curr_tc) == 3 && unique_stim_classes(curr_sc) < 5 % SCC
             all_trials = (condition_master.stim_class==99 & condition_master.task_class==unique_task_classes(curr_tc));
         else
             all_trials = (condition_master.stim_class==unique_stim_classes(curr_sc) & condition_master.task_class==unique_task_classes(curr_tc));
@@ -494,19 +500,19 @@ for curr_sc = 1:(length(unique_stim_classes)-1)
             
             if ~isnan(nr_blocks)
                 if mod(nr_blocks,1)~=0
-                    tmp = repelem(1:nr_blocks,params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc))); 
+                    tmp = repelem(1:nr_blocks,params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc)));
                     tmp = cat(2,tmp, repmat(tmp(end)+1, 1, mod(nr_blocks,1)*params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc))));
                 else
                     tmp = repelem(1:nr_blocks,params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc)));
                 end
-            
+                
                 if length(tmp) > length(unused_idx) % trim if needed
                     tmp = tmp(1:length(unused_idx))';
                 elseif length(unused_idx) > length(tmp)
                     error('wtf')
                 end
                 condition_master.stim_class_unique_block_nr(unused_idx) = tmp;
-
+                
                 % update unique_trial_nr
                 nr_unique_condition_repeats = length(unused_idx)/params.exp.nr_unique_trials_per_crossing(unique_stim_classes(curr_sc),unique_task_classes(curr_tc));
                 tmp = repmat(1:params.exp.nr_unique_trials_per_crossing(unique_stim_classes(curr_sc),unique_task_classes(curr_tc)),1,ceil(nr_unique_condition_repeats));
@@ -516,7 +522,7 @@ for curr_sc = 1:(length(unique_stim_classes)-1)
                     error('wtf')
                 end
                 condition_master.unique_trial_nr(unused_idx) = tmp;
-
+                
                 % update trial_nr
                 tmp = repmat(1:params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc)),1,ceil(nr_blocks));
                 if length(tmp) > length(unused_idx) % trim if needed
@@ -535,7 +541,7 @@ for curr_sc = 1:(length(unique_stim_classes)-1)
             
             if ~isnan(nr_blocks)
                 if mod(nr_blocks,1)~=0
-                    tmp = repelem(start_block_nr:(start_block_nr+nr_blocks-1),params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc))); 
+                    tmp = repelem(start_block_nr:(start_block_nr+nr_blocks-1),params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc)));
                     tmp = cat(2,tmp, repmat(tmp(end)+1, 1, mod(nr_blocks,1)*params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc))));
                 else
                     tmp = repelem(start_block_nr:(start_block_nr+nr_blocks-1),params.exp.nr_trials_per_block(unique_stim_classes(curr_sc),unique_task_classes(curr_tc)));
